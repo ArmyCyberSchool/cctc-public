d1b6217ba0fb9ab9b1471074b65c1b2740609204
Sarah Rees <sarah.d.rees.ctr@mail.mil>
2018-01-03 08:46:45 -0500

    updated comments, changed print statement back to python 2.7 format.

--- networking/additional_resources/RAWSOCK.py
+++ networking/additional_resources/RAWSOCK.py
@@ -1,11 +1,11 @@
 import socket, sys, binascii
 from struct import *
  
-# Create a raw socket
+# Create a raw socket. Note that the print statement is per python 2.7. If you are running this with python 3.0 you will need to format accourdingly.
 try:
     s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_RAW)
 except socket.error as msg:
-    print ('Socket creation failed!. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
+    print 'Socket creation failed!. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
     sys.exit()
      
 # Construct the packet. We are going to add the IP header, then just stick some data in it since a raw socket can handle this.
@@ -26,7 +26,7 @@ ip_check = 0    # kernel will fill the correct checksum
 ip_saddr = socket.inet_aton ( source_ip )
 ip_daddr = socket.inet_aton ( dest_ip )
  
-# The ! in the pack format string means network order while the code following specifies how to store the info...B=Byte, H=Half-word, 4s=four bytes (a str is a byte long)
+# This portion creates the header by packing the above variables into a structure. The ! in the string means network order, while the code following specifies how to store the info. These "?"'s should indicate the size of the field that the information contained in the corresponding variables should be stored in. The "?"'s should be replaces with B, H or 4s. B=Byte, H=Half-word, 4s=four bytes (a str is a byte long)
 ip_header = pack('!????????????' , ip_ver_ihl, ip_tos, ip_len, ip_id, ip_frag, ip_ttl, ip_proto, ip_check, ip_saddr, ip_daddr)
  
 # Your custom protocol fields or data. We are going to just insert data here. Add your message where the "?" is. Ensure you obfuscate it though...don't want any clear text messages being spotted! You can use a function from the binascii module as in this example, or you can use something else.
